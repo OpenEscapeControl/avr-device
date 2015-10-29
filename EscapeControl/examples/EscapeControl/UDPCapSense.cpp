8782cf5e0323724d52430162f44855c548656f5d
Sergey Volodin <etoestja@yandex.ru>
2015-10-29 22:30:36 +0300

    Add tx and rx pins to ANS_CAPSENSE_DO

--- EscapeControl/examples/EscapeControl/UDPCapSense.cpp
+++ EscapeControl/examples/EscapeControl/UDPCapSense.cpp
@@ -4,16 +4,18 @@
 
 bool processCommandCapSense(uint8_t* data, uint8_t len)
 {
-    if(data[0] == CMD_CAPSENSE_DO && len == 4)
+    if(data[0] == CMD_CAPSENSE_DO && len >= 5)
     {
         uint8_t* dst = UDPProcessor::getUDPPtr();
         uint32_t res = CapSense(data[1], data[2], data[3], data[3]);
         dst[0] = ANS_CAPSENSE_DO;
-        dst[1] = res & 0xff;
-        dst[2] = (res >> 8) & 0xff;
-        dst[3] = (res >> 16) & 0xff;
-        dst[4] = (res >> 24) & 0xff;
-        UDPProcessor::tx(5);
+        dst[1] = data[1];
+        dst[2] = data[2];
+        dst[3] = res & 0xff;
+        dst[4] = (res >> 8) & 0xff;
+        dst[5] = (res >> 16) & 0xff;
+        dst[6] = (res >> 24) & 0xff;
+        UDPProcessor::tx(7);
         return(true);
     }
     return(false);
