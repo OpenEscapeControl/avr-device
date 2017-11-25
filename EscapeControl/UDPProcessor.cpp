90b7ef516fa7bfea1bac112a44818421c0c28a8b
Sergei Volodin <etoestja@yandex.ru>
2017-11-26 00:02:27 +0100

    Add mocked version of UDPProcessor (UART)

--- EscapeControl/UDPProcessor.cpp
+++ EscapeControl/UDPProcessor.cpp
@@ -1,78 +1,34 @@
 #include "UDPProcessor.h"
-#include <stdio.h>
 
-void rxCallback(unsigned len, const char* buf);
+char UDPProcessor::buf[MAX_LEN];
 
-// SHOULD NOT BE < 400
-uint8_t Ethernet::buffer[BUFSIZE];
-
-uint8_t UDPProcessor::remote_mac_[] = {0xAA, 0x00, 0x00, 0x00, 0x00, 0x00};
-uint8_t UDPProcessor::remote_ip_[] = {192, 168, 100, 1};
-unsigned UDPProcessor::local_tx_port_ = 59001;
-
-uint8_t UDPProcessor::mac_[] = {0xAA, 0x00, 0x00, 0x00, 0x01, 0x00};
-uint8_t UDPProcessor::ip_[] = {192, 168, 100, 10};
-uint8_t UDPProcessor::mask_[] = {255, 255, 255, 0};
-unsigned UDPProcessor::local_rx_port_ = 49900;
-unsigned UDPProcessor::remote_port_ = 0;
-
-void rxCallback1(unsigned port, unsigned char ip[], const char *data, unsigned len)
+UDPProcessor::UDPProcessor(const uint8_t physical_device)
 {
-    rxCallback(len, data);
-}
-
-
-UDPProcessor::UDPProcessor(uint8_t physical_device)
-{
-    mac_[5] = 10 + physical_device;
-    ip_[3] = 10 + physical_device;
-    char buf[10];
-    sprintf(buf, "2%02d%02d", physical_device, 1);
-    remote_port_ = atoi(buf);
-    sprintf(buf, "1%02d%02d", physical_device, 1);
-    local_rx_port_ = atoi(buf);
+    device = physical_device;
+    buf[0] = 0;
 }
 
 int UDPProcessor::begin()
 {
-    if(!ether.begin(sizeof Ethernet::buffer, mac_))
-    {
-        return(-1);
-    }
-
-    ether.staticSetup (ip_, NULL, NULL, mask_);
-
-    setCallBack(rxCallback1);
-
+    Serial.begin(115200);
     return(0);
-}
-
-void UDPProcessor::tx(const char *msg, int size)
-{
-    ether.sendUdp(msg, size, local_tx_port_, remote_ip_, remote_port_, remote_mac_);
-}
-
-void UDPProcessor::tx(const char *cstr)
-{
-    tx(cstr, strlen(cstr) + 1);
-}
-
-void UDPProcessor::setCallBack(UdpServerCallback callback)
-{
-    ether.udpServerListenOnPort(callback, local_rx_port_);
 }
 
 void UDPProcessor::update()
 {
-    ether.packetLoop(ether.packetReceive());
+    int len = 0;
+    while (Serial.available() > 0)
+        buf[len++] = Serial.read();
+    rxCallback(len, buf);
 }
 
-uint8_t *UDPProcessor::getUDPPtr()
+uint8_t* UDPProcessor::getUDPPtr()
 {
-    return(ether.getUDPPointer());
+    return((uint8_t*) buf);
 }
 
 void UDPProcessor::tx(int size)
 {
-    ether.sendUDPPTR(size, local_tx_port_, remote_ip_, remote_port_, remote_mac_);
+    buf[size + 1] = 0;
+    Serial.write((unsigned char*) buf, size);
 }
--- EscapeControl/UDPProcessor.h
+++ EscapeControl/UDPProcessor.h
@@ -1,36 +1,26 @@
-#ifndef UDPPROCESSOR_H
-#define UDPPROCESSOR_H
+#ifndef UART_URP_PROCESSOR
+#define UART_URP_PROCESSOR
 
-#include "EtherCard.h"
-#include "udp_config.h"
+#include "Arduino.h"
+#include <inttypes.h>
+
+void rxCallback(unsigned len, const char *data);
+
+#define MAX_LEN 1000
 
 class UDPProcessor
 {
 private:
-    static const unsigned IP_BYTES = 4;
-
     static PROGMEM char const hostname_[];
-    static uint8_t mac_[MAC_BYTES];
-    static uint8_t remote_mac_[MAC_BYTES];
-    static unsigned local_tx_port_;
+    static char buf[MAX_LEN];
+    int device;
 
-    static uint8_t remote_ip_[IP_BYTES];
-    static uint8_t ip_[IP_BYTES];
-    static uint8_t mask_[IP_BYTES];
-    static unsigned remote_port_;
-
-    static unsigned local_rx_port_;
-
-    static uint16_t buffer_counter_;
 public:
     UDPProcessor(const uint8_t physical_device);
     static int begin();
-    static void tx(const char* msg, int size);
-    static void tx(const char* cstr);
-    static void setCallBack(UdpServerCallback callback);
     static void update();
     static uint8_t* getUDPPtr();
     static void tx(int size);
 };
 
-#endif
+#endif // UART_UDP_PROCESSOR
