3869a5ab8614f733abc8865aca0c3fb1aaa42b19
Sergei Volodin <etoestja@yandex.ru>
2017-12-13 20:07:18 +0100

    Allocate TM objects on compile time

--- project/UDPTM1637.cpp
+++ project/UDPTM1637.cpp
@@ -4,18 +4,10 @@
 #include "UDPProcessor.h"
 #include <inttypes.h>
 
-TM1637Display* displays[MAX_TM1637_N];
-
-bool initialized = false;
+TM1637Display displays[MAX_TM1637_N];
 
 bool processCommandTM1637(uint8_t* data, uint8_t len)
 {
-    if(!initialized)
-    {
-        for(int i = 0; i < MAX_TM1637_N; i++)
-            displays[i] = NULL;
-        initialized = true;
-    }
     if(len < 2 || data[1] >= MAX_TM1637_N)
         return(0);
     if(data[0] == CMD_TM1637_INIT && len >= 3)
@@ -24,28 +16,24 @@ bool processCommandTM1637(uint8_t* data, uint8_t len)
         uint8_t id = data[1];
         uint8_t clk_n = data[2];
         uint8_t dio_n = data[3];
-        if(displays[id] != NULL)
-            delete displays[id];
-        displays[id] = new TM1637Display(clk_n, dio_n);
+        displays[id].init(clk_n, dio_n);
+
+//        UDPProcessor::tx(len);
 
         return(1);
     }
     else if(data[0] == CMD_TM1637_DISPLAYDEC && len >= 2)
     {
-        TM1637Display* disp = displays[data[1]];
-
-        if(disp == NULL)
-            return(1);
-
-        disp->setBrightness(0x0f);
-
         // proto: tm_n digit1 digit2 digit3 digit4
+        uint8_t id = data[1];
+        displays[id].setBrightness(0x0f);
 
         uint8_t digits[4];
         for(int i = 0; i < 4; i++)
-            digits[i] = disp->encodeDigit(data[2 + i]);
+            digits[i] = displays[id].encodeDigit(data[2 + i]);
 
-        disp->setSegments(digits);
+        displays[id].setSegments(digits);
+//        UDPProcessor::tx(len);
 
         return(1);
     }
